4bd2afc424656ccacebd331d81588380b8d0fc6d
dev.mateus <mateusdecamposturkoco@gmail.com>
2025-02-11 22:15:47 -0300

    exes

--- exe04.py
+++ exe04.py
@@ -1,13 +1,12 @@
 #Peça ao usuário para inserir sua cor favorita. Se ele digitar "vermelho",
 #"VERMELHO" ou "Vermelho" exibem a mensagem "Eu também gosto de vermelho", caso contrário, exibem a mensagem "Eu não gosto de [cor], eu prefiro vermelho".
 cor = (input("Digite sua cor favorita: "))
-if cor == 'vermelho':
+cor1 = cor.title()
+if cor == 'Vermelho':
     print("Eu também gosto de vermelho")
-if cor == 'Vermelho:':
-     print("Eu também gosto de vermelho")
+    print("Mateus De Campos Turkoco")
 else:
-     if cor == 'VERMELHO':
-           print("Eu também gosto de vermelho")     
-
+    print("Eu não gosto desta", cor1 ,"cor:")
+    print("Mateus De Campos Turkoco")
 
     
